dff0c33420bfae131fffdb5826a28cd9e41c2b42
47652_shubham <to.shubhpatil@gmail.com>
2025-02-09 16:37:29 +0530

    added Navbar

--- src/App.js
+++ src/App.js
@@ -6,25 +6,26 @@ import Navbar from './components/navbar'
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [showApp, setShowApp] = useState(false);
-  useEffect(() => {
-      const timer = setTimeout(() => {
-          setShowApp(true);
-      }, 3500); // 3-second delay
+  // const [showApp, setShowApp] = useState(false);
+  // useEffect(() => {
+  //     const timer = setTimeout(() => {
+  //         setShowApp(true);
+  //     }, 3500);
 
-      return () => clearTimeout(timer);
-  }, []);
+  //     return () => clearTimeout(timer);
+  // }, []);
   return (
     <>
-    <LoadingAnimation/>
-    {showApp &&<div className="App">
+    {/* <LoadingAnimation/> */}
+    {/* {showApp &&<div className="App"> */}
+    <div className="App">
       <BrowserRouter>
       <Navbar/>
         <Routes>
           <Route path='/' element={<Landing/>}></Route>
         </Routes>
       </BrowserRouter>
-    </div>}
+    </div>
     </>
   );
 }
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,17 +1,36 @@
 import './index.css'
 import { logo } from '../svg'
 import LoadingAnimation from '../LoadingAnimation'
+import { Link } from 'react-router-dom'
+import {fileDownload } from '../svg'
 
 const Navbar = () => {
 
     return (
         <>
-            <div className="navbar">
+            <nav className="navbar">
                 <div className='logocontainer'>
                     <div className="Slogo">s</div>
                     {logo}
                 </div>
-            </div>
+                <div className="navlinks_container">
+                    <ol className='navlinks'>
+                        <li className='nav_link'> <Link>about</Link> </li>
+                        <li className='nav_link'> <Link>about</Link> </li>
+                        <li className='nav_link'> <Link>about</Link> </li>
+                        <li className='nav_link'> <Link>about</Link> </li>
+                        <li className='nav_link btn_li'> <button className='resumeBTN'>
+                            <div className="navbtnsvg" id="resume">
+                                {fileDownload}
+                            </div>
+                            <div className="navbtntext">
+                            Resume
+                            </div>
+                            </button> </li>
+                    </ol>
+
+                </div>
+            </nav>
         </>
     )
 }
