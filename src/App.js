534d1d45180ba8644fef06bc423ef230e840c8e2
47652_shubham <to.shubhpatil@gmail.com>
2025-02-10 23:58:45 +0530

    smoothed out animation

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   // }, []);
   return (
     <>
-    {/* <LoadingAnimation/> */}
+    <LoadingAnimation/>
     {/* {showApp &&<div className="App"> */}
     <div className="App">
       <BrowserRouter>
--- src/components/LoadingAnimation/index.jsx
+++ src/components/LoadingAnimation/index.jsx
@@ -10,14 +10,15 @@ const LoadingAnimation = ()=>{
             <text x="87" y="115" className="text">S
             </text>
             <polygon points="100,50 60,75 60,125 100,150 140,125 140,75 100,50" fill="none" stroke="lightblue"
-                strokeWidth="5" strokeDasharray="1000" strokeDashoffset="1000" strokeLinecap="round">
+                strokeWidth="5" strokeDasharray="290" strokeDashoffset="290" strokeLinecap="round">
                 <animate 
                 attributeName="stroke-dashoffset" 
-                from="1000" 
-                to="0" dur="1.1s" 
+                from="290" 
+                to="0" dur="1.5s"
+                begin="0.3s"
                 repeatCount="1" 
                 keyTimes="0;1"
-                keySplines="0.75, 0, 1, 1"
+                keySplines="0.77 0 0.18 1"
                 calcMode="spline"
                 fill="freeze">
                 </animate>
