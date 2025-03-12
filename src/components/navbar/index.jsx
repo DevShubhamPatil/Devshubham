646622f2772edd9e8a0a9797cbd86fecec2f814e
47652_shubham <to.shubhpatil@gmail.com>
2025-03-13 03:33:41 +0530

    added and tweaked hover animation on navbar links still a biot buggy but will push it

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,7 +2,7 @@ import './index.css'
 import { hamb, logo } from '../svg'
 import { Link } from 'react-scroll'
 import { fileDownload } from '../svg'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 
 const Navbar = (props) => {
@@ -10,7 +10,8 @@ const Navbar = (props) => {
     const [navResumeVisible, setNavResumeVisible] = useState(false);
     const [vw, setVW] = useState(window.innerWidth)
     const [vh, setVH] = useState(window.innerHeight)
-    const [section, setSection] = useState(-1)
+    // const [section, setSection] = useState(-1)
+    const section = useRef(-1)
 
     useEffect(() => {
         const setDiamentions = () => {
@@ -18,14 +19,11 @@ const Navbar = (props) => {
             setVH(window.innerHeight)
         }
         const handleScroll = () => {
-            // console.log('vh: ' + vh + ' scroll: ' + window.scrollY + ' sec: ' + window.scrollY / vh + ' floor: ' + Math.floor(window.scrollY / vh))
-            var actvbg = document.getElementById('actvbg')
-            var actvbgX = actvbg.getBoundingClientRect().left
-            var cursection = Math.floor((window.scrollY / vh)+0.45)
-            if(section != cursection) {
-                setSection(cursection)
-                actvbg.style.setProperty('--length',`${document.getElementById(cursection).getBoundingClientRect().width+20}px`)
-                actvbg.style.setProperty('--x',`${document.getElementById(cursection).getBoundingClientRect().left }px`)
+        
+            var cursection = Math.floor((window.scrollY / vh) + 0.45)
+            if (section != cursection) {
+                handleMouseHover(document.getElementById(section.current<0? 0:section.current),document.getElementById(cursection))
+                section.current = cursection
             }
             requestAnimationFrame(() => {
                 setScrolled(window.scrollY > 10);
@@ -43,6 +41,30 @@ const Navbar = (props) => {
         };
     }, []);
 
+    const handleMouseHover = (source , destination) => {
+        if (source === destination){
+            return
+        }
+
+        const destId = Number(destination.id)
+        const sourceId = Number(source.id)
+        const destWidth = destination.getBoundingClientRect().width +30
+        const destinationPosition = destination.getBoundingClientRect().left;
+        const jump = destId - sourceId;
+        const extra = (15 + Math.abs(jump)) * jump;
+        const startpoint =document.getElementById('actvbg').getBoundingClientRect().left
+
+        const animation = [ { left: startpoint+'px', offset:0},
+                            { left: (destinationPosition + extra) + 'px', width: (destWidth)+'px',offset:0.3 },
+                            { left: (destinationPosition + extra) + 'px', width: (destWidth-20)+'px',offset:0.37 },
+                            { left: (destinationPosition - (extra / 2)) + 'px', width: destWidth+'px',offset:0.6 },
+                            { left: (destinationPosition + (extra / 4)) + 'px', width: destWidth+'px',offset:0.85},
+                            { left: destinationPosition+'px' , width: destWidth+'px', offset:1}]
+        const options = {   duration:800,
+                            fill: 'forwards'
+                        }
+        document.getElementById('actvbg').animate(animation,options)
+    }
 
     return (
         <>
@@ -53,11 +75,10 @@ const Navbar = (props) => {
                 </div>
                 <div className="navlinks_container">
                     <ol className='navlinks'>
-                        <div className="active" id='actvbg'></div>
-                        <li className='nav_link' id='0' data-active='false'> <Link to="home" smooth={true} duration={400}>About</Link> </li>
-                        <li className='nav_link' id='1' data-active='flase'> <Link to="Skills" smooth={true} duration={400}>Tech Skills</Link> </li>
-                        <li className='nav_link' id='2' data-active='false'> <Link to="contactme" smooth={true} duration={400}>ContactMe</Link> </li>
-                        <li className='nav_link' id='3' data-active='false'> <Link to="about" smooth={true} duration={400}>about</Link> </li>
+                        <li className='nav_link' onMouseEnter={(e)=>handleMouseHover( document.getElementById(section.current<0? 0:section.current), e.currentTarget)} onMouseLeave={(e)=>handleMouseHover(e.currentTarget,document.getElementById(section.current<0? 0:section.current))} id='0' data-active='false'> <Link to="home" smooth={true} duration={350} >About</Link> </li>
+                        <li className='nav_link' onMouseEnter={(e)=>handleMouseHover( document.getElementById(section.current<0? 0:section.current), e.currentTarget)} onMouseLeave={(e)=>handleMouseHover(e.currentTarget,document.getElementById(section.current<0? 0:section.current))} id='1' data-active='flase'> <Link to="Skills" smooth={true} duration={350}>Tech Skills</Link> </li>
+                        <li className='nav_link' onMouseEnter={(e)=>handleMouseHover( document.getElementById(section.current<0? 0:section.current), e.currentTarget)} onMouseLeave={(e)=>handleMouseHover(e.currentTarget,document.getElementById(section.current<0? 0:section.current))} id='2' data-active='false'> <Link to="contactme" smooth={true} duration={350}>ContactMe</Link> </li>
+                        <li className='nav_link' onMouseEnter={(e)=>handleMouseHover( document.getElementById(section.current<0? 0:section.current), e.currentTarget)} onMouseLeave={(e)=>handleMouseHover(e.currentTarget,document.getElementById(section.current<0? 0:section.current))} id='3' data-active='false'> <Link to="about" smooth={true} duration={350}>about</Link> </li>
                         <li className={`nav_link btn_li ${navResumeVisible ? "" : "invisible"}`}>
                             <button className='resumeBTN' onClick={props.downloadResume}>
                                 <div className="navbtnsvg" id="resume">
@@ -72,6 +93,7 @@ const Navbar = (props) => {
                                 {hamb}
                             </div>
                         </li>
+                        <div className="active" id='actvbg'></div>
 
                     </ol>
                 </div>
