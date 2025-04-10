dd914099dc310097eedf65ac96d0d64a447c6630
47652_shubham <to.shubhpatil@gmail.com>
2025-04-10 21:53:08 +0530

    Changed Experiance section mobile view

--- src/components/Experiance/index.jsx
+++ src/components/Experiance/index.jsx
@@ -1,8 +1,12 @@
 import './index.css'
 
 const Experiance = () => {
-    const data = `• Led the development of a message parsing application to automate the XML messages generation from text files to be processed by downstream application, Improving the trade processing speed by 20%.\n• Enhanced security compliance by integrating Vault to eliminate hardcoded passwords and certificates by 100%, collaborating with cross-functional teams to strengthen security protocols and mitigate risks."\n• Reduced system downtime by 25% with problem-solving and teamwork with colleagues and cross-functional teams to resolve critical production issues, fix the disaster recovery (DR) of an application, and remove Vulnerable dependencies from applications.\n• Lowered operational costs by 15% by leading the decommissioning of legacy applications and infrastructure\n• Ensured regulatory compliance by archiving data, mitigating potential legal risks through attention to detail.`
+    const data = `• Led the development of a message parsing application to automate the XML messages generation from text files to be processed by downstream application, Improving the trade processing speed by 20%.\n• Enhanced security compliance by integrating Vault to eliminate hardcoded passwords and certificates by 100%, collaborating with cross-functional teams to strengthen security protocols and mitigate risks.\n• Reduced system downtime by 25% with problem-solving and teamwork with colleagues and cross-functional teams to resolve critical production issues, fix the disaster recovery (DR) of an application, and remove Vulnerable dependencies from applications.\n• Lowered operational costs by 15% by leading the decommissioning of legacy applications and infrastructure\n• Ensured regulatory compliance by archiving data, mitigating potential legal risks through attention to detail.`
 
+    const togglereadmore = () => {
+        const cs = document.getElementById('cs')
+        cs.classList.toggle('preclass')
+    }
     return (
         <div className="ExpContainer">
             <div className="skillheading">Experiance</div>
@@ -13,20 +17,20 @@ const Experiance = () => {
 
                     <div className="expcont">
                         <div>
-                            <span className="line1" style={{ lineHeight: '1rem', padding: '5px', marginLeft: '-15px' }}>
+                            <span className="line1" style={{fontSize:'1.3rem', lineHeight: '1rem', padding: '5px', marginLeft: '-15px' }}>
                                 Credit Suisse (UBS) :
                             </span>
-                            <span>
+                            <span style={{display:'inline-block'}}>
                                 June 2022 - now
                             </span>
                         </div>
                         <div>
-                            <pre style={{ textAlign: "justify" }} >{data}</pre>
+                            <pre id='cs' className='preclass' onClick={togglereadmore}>{data}</pre>
                             <div>
-                                <span className="line1" style={{ lineHeight: '1.2rem', padding: '0px', marginRight:'5px' }}>
+                                <span className="line1" style={{ fontSize:'1.15rem', lineHeight: '1rem', padding: '0px', marginRight: '5px' }}>
                                     Tech Stack :
                                 </span>
-                                 Java, Spring Boot, Oracle, React, C#, .NET, APIs, Microservices, Pub/Sub Docker Agile, CI/CD.
+                                Java, Spring Boot, Oracle, React, C#, .NET, APIs, Microservices, Pub/Sub, Docker, CI/CD, Agile.
                             </div>
                         </div>
 
